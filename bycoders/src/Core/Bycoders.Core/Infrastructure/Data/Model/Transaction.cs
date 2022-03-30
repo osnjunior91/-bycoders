608506514ea700795dd9b34c3ed0e7e5d824ad61
Oswaldo Junior <osnjunior91@gmail.com>
2022-03-30 16:03:12 -0300

    Create fast fail validation in object transaction

--- bycoders/src/Core/Bycoders.Core/Infrastructure/Data/Model/Transaction.cs
+++ bycoders/src/Core/Bycoders.Core/Infrastructure/Data/Model/Transaction.cs
@@ -6,6 +6,33 @@ namespace Bycoders.Domain.Infrastructure.Data.Model
 {
     public class Transaction : Entity
     {
+        public Transaction(int typeTransactionCode, DateTime date, float value, string personDocument, 
+            string cardNumber, string storeManager, string storeName, DateTime createdAt)
+        {
+            if (typeTransactionCode == 0)
+                throw new ArgumentNullException(nameof(typeTransactionCode));
+            TypeTransactionCode = typeTransactionCode;
+            if (date == DateTime.MinValue)
+                throw new ArgumentNullException(nameof(date));
+            Date = date;
+            if (value == float.MinValue)
+                throw new ArgumentNullException(nameof(value));
+            Value = value;
+            if (personDocument.Length < 11)
+                throw new ArgumentNullException(nameof(personDocument));
+            PersonDocument = personDocument;
+            if (cardNumber.Length < 12)
+                throw new ArgumentNullException(nameof(cardNumber));
+            CardNumber = cardNumber;
+            if (storeManager.Length < 1)
+                throw new ArgumentNullException(nameof(storeManager));
+            StoreManager = storeManager;
+            if (storeName.Length < 1)
+                throw new ArgumentNullException(nameof(storeName));
+            StoreName = storeName;
+            CreatedAt = createdAt;
+        }
+
         public int TypeTransactionCode { get; set; }
         public TypeTransaction Type { get; set; }
         public DateTime Date { get; set; }
@@ -17,16 +44,9 @@ namespace Bycoders.Domain.Infrastructure.Data.Model
 
         public static explicit operator Transaction(string line)
         {
-            var retorno = new Transaction();
-            retorno.TypeTransactionCode = int.Parse(line.Substring(0, 1));
-            retorno.Date = line.Substring(1, 8).CNABDate(line.Substring(42, 6));
-            retorno.Value = float.Parse(line.Substring(9, 10)) / 100;
-            retorno.PersonDocument = line.Substring(19, 11).Trim();
-            retorno.CardNumber = line.Substring(30, 12).Trim();
-            retorno.StoreManager = line.Substring(48, 14).Trim();
-            retorno.StoreName = line.Substring(62).Trim();
-            retorno.CreatedAt = DateTime.Now;
-            return retorno;
+            return new Transaction(int.Parse(line.Substring(0, 1)), line.Substring(1, 8).CNABDate(line.Substring(42, 6)), 
+                float.Parse(line.Substring(9, 10)) / 100, line.Substring(19, 11).Trim(), line.Substring(30, 12).Trim(),
+                line.Substring(48, 14).Trim(), line.Substring(62).Trim(), DateTime.Now);
         }
     }
 }
